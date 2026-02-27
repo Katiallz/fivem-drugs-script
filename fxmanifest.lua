76c43c03dd2706152a6555a930cdaedc7c92291e
Katiallz <furaogamerbr@gmail.com>
2026-02-26 23:07:14 -0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "bodacious"
+game "gta5"
+lua54 "yes"
+
+author "Katiaallz"
+description "💊 Sistema de Venda de Drogas para FiveM (vRP) | Venda para NPC com bloqueio de cargos e controle anti-spam."
+version "1.0.0"
+
+client_scripts {
+	"client-side/drugs.lua"
+}
+
+server_scripts {
+	"server-side/core.lua"
+}
